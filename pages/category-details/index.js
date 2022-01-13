47817367cbd0bf0f5667e6d195e9415486806220
mmdsh-programmer <programmer.mmdsh@gmail.com>
2022-01-13 10:44:42 +0330

    add some motion to titles and elements

--- pages/category-details/index.js
+++ pages/category-details/index.js
@@ -1,11 +1,108 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Head from "next/head";
 import styles from "./CategoryDetails.module.css";
 import { MoreButton } from "components/MoreButton";
 import { motion } from "framer-motion";
+import { gsap, Power4, Power2 } from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import SplitType from "split-type";
 
 export default function CategoryDetails() {
+  gsap.registerPlugin(ScrollTrigger);
+  const mainTitleRef = useRef();
+  const headerSubtitleRef = useRef();
+  const bannerDescriptionRef = useRef();
+  const counterHolderRef = useRef();
+
+  const heroAnimation = () => {
+    new SplitType(mainTitleRef.current, {
+      linesClass: "split-child",
+    });
+
+    new SplitType(headerSubtitleRef.current, {
+      linesClass: "split-child",
+    });
+
+    const timeLine = gsap.timeline({
+      scrollTrigger: {
+        trigger: mainTitleRef.current,
+        start: "center bottom",
+      },
+    });
+    timeLine
+      .from(".main-title .word", {
+        duration: 1.2,
+        delay: 0.5,
+        y: 500,
+        ease: Power4.easeOut,
+      })
+      .from(
+        ".header-subtitle .word",
+        {
+          duration: 1.3,
+          y: 500,
+          ease: Power2.easeOut,
+        },
+        "0.5"
+      );
+  };
+
+  const bannerDescriptionAnimation = () => {
+    new SplitType(bannerDescriptionRef.current, {
+      linesClass: "split-child",
+    });
+
+    const timeLine = gsap.timeline({
+      scrollTrigger: {
+        trigger: bannerDescriptionRef.current,
+        start: "top bottom",
+      },
+    });
+
+    timeLine.from(".banner-description .word", {
+      duration: 1.2,
+      delay: 0.5,
+      y: 500,
+      stagger: 0.01,
+      ease: Power4.easeOut,
+    });
+  };
+
+  const catalogueCounterAnimation = () => {
+    const counters = counterHolderRef.current.children;
+    const timeLine = gsap.timeline({
+      scrollTrigger: {
+        trigger: counterHolderRef.current,
+        start: "top center - 30%",
+      },
+    });
+
+    timeLine
+      .to(".counter-separator", {
+        transformOrigin: "% 50%",
+        scaleY: 1,
+        duration: 1,
+        ease: Power2.easeOut,
+      })
+      .to(
+        ".mask-layer",
+        {
+          transformOrigin: "50% 0%",
+          scaleX: 0,
+          duration: 1,
+          ease: Power2.easeOut,
+        },
+        "0.5"
+      );
+  };
+
+  useEffect(() => {
+    heroAnimation();
+    bannerDescriptionAnimation();
+    catalogueCounterAnimation();
+  }, []);
+
   return (
     <>
       <Head>
@@ -24,8 +121,16 @@ export default function CategoryDetails() {
         <section className={styles["hero"]}>
           <div className="container">
             <header className={styles["category-header"]}>
-              <h1 className={styles["header-title"]}>Decorative Mirror</h1>
-              <p className={styles["header-subtitle"]}>
+              <h1
+                className={`${styles["header-title"]} main-title`}
+                ref={mainTitleRef}
+              >
+                Decorative Mirror
+              </h1>
+              <p
+                className={`${styles["header-subtitle"]} header-subtitle`}
+                ref={headerSubtitleRef}
+              >
                 Iran Glass Technology with more than 40 years of experience and
                 with the aim of meeting the needs of customers and improving the
                 quality of products
@@ -40,7 +145,10 @@ export default function CategoryDetails() {
             />
           </div>
 
-          <p className={styles["description"]}>
+          <p
+            className={`${styles["description"]} banner-description`}
+            ref={bannerDescriptionRef}
+          >
             Iran Glass Technology with more than 40 years of experience and with
             the aim of meeting the needs of customers and improving the quality
             of products since 2001 has changed its name and created the Iranian
@@ -51,14 +159,18 @@ export default function CategoryDetails() {
         <section className={styles["catalogue"]}>
           <div className={`container ${styles["container"]}`}>
             <div
-              className={`row ml-lg-20 mr-lg-20 wrap ${styles["catalogue-counter"]}`}
+              className={`row ml-lg-20 mr-lg-20 wrap ${styles["catalogue-counter"]} catalogue-counter`}
+              ref={counterHolderRef}
             >
               <div
                 className={`col ${styles["col-4"]} row justify-start align-start`}
               >
-                <div className="row align-end">
+                <div className="row align-end p-relative">
+                  <div className={`${styles["mask-layer"]} mask-layer`}></div>
                   <span className={styles["counter-number"]}>48</span>
-                  <span className={styles["counter-separator"]}></span>
+                  <span
+                    className={`${styles["counter-separator"]} counter-separator`}
+                  ></span>
                   <h4 className={`inline-block ${styles["counter-title"]}`}>
                     Wall mirors
                   </h4>
@@ -67,9 +179,12 @@ export default function CategoryDetails() {
               <div
                 className={`col ${styles["col-4"]} row justify-center align-center`}
               >
-                <div className="row align-end">
+                <div className="row align-end p-relative">
+                  <div className={`${styles["mask-layer"]} mask-layer`}></div>
                   <span className={styles["counter-number"]}>6</span>
-                  <span className={styles["counter-separator"]}></span>
+                  <span
+                    className={`${styles["counter-separator"]} counter-separator`}
+                  ></span>
                   <h4 className={`inline-block ${styles["counter-title"]}`}>
                     Miror tables
                   </h4>
@@ -78,9 +193,12 @@ export default function CategoryDetails() {
               <div
                 className={`col ${styles["col-4"]} row justify-end align-end`}
               >
-                <div className="row align-end">
+                <div className="row align-end p-relative">
+                  <div className={`${styles["mask-layer"]} mask-layer`}></div>
                   <span className={styles["counter-number"]}>10</span>
-                  <span className={styles["counter-separator"]}></span>
+                  <span
+                    className={`${styles["counter-separator"]} counter-separator`}
+                  ></span>
                   <h4 className={`inline-block ${styles["counter-title"]}`}>
                     Miror consuls
                   </h4>
